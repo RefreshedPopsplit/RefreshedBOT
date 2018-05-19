769d522b5312b3f86cb15eb8112c7b6095eed482
RefreshedPopsplit <38994528+RefreshedPopsplit@users.noreply.github.com>
2018-05-19 15:03:21 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,3 +6,6 @@ bot = commands.Bot(command_prefix=prefix)
 async def hello():
     await bot.say("Hello :wave:" )
 bot.run("NDQ2Nzc0NTg0NjM3NjUyOTky.DeH-LA.NERXRC5-flgNWtw6sMqxNwhSnUc")
+@bot.command
+async def invite():
+    await bot.say("https://discordapp.com/oauth2/authorize?client_id=446774584637652992&scope=bot")
